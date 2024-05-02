df7c3cc561d1eca8a6a1d555aee3f4acfa8c60fd
gabriellama <gabriellamalvares@gmail.com>
2024-05-02 17:39:35 -0300

    Conta corrente e conta poupança

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -1,20 +1,31 @@
 package conta;
 
 import java.util.Scanner;
+
 import conta.model.Conta;
+import conta.model.ContaPoupanca;
 import conta.util.Cores;
+import conta.model.ContaCorrente;
 
 public class Menu {
 
 	public static void main(String[] args) {
+			
+		// teste da classe conta corrente
+		Conta cc1 = new ContaCorrente(2, 123, 1, "Mariana", 15000.0f, 1000.0f);
+		cc1.visualizar();
+		cc1.sacar(12000.0f);
+		cc1.visualizar();
+		cc1.depositar(5000.0f);
+		cc1.visualizar();
 		
-		// teste
-		Conta c1 = new Conta(1, 123, 1, "Adriana", 10000.0f);
-		c1.visualizar();
-		c1.sacar(12000.0f);
-		c1.visualizar();
-		c1.depositar(5000.0f);
-		c1.visualizar();
+		// teste da classe conta poupança
+		Conta cp1 = new ContaPoupanca(3, 123, 1, "Victor", 100000.0f, 15);
+		cp1.visualizar();
+		cp1.sacar(1000.0f);
+		cp1.visualizar();
+		cp1.depositar(5000.0f);
+		cp1.visualizar();
 		
 		Scanner leia = new Scanner(System.in);
 		int opcao;
