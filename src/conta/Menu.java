f8f92d6c0022d41d3fa746355c22a8f45f12c117
gabriellama <gabriellamalvares@gmail.com>
2024-05-04 23:15:58 -0300

    retirada do teste no menu

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -12,22 +12,6 @@ import conta.model.ContaCorrente;
 public class Menu {
 
 	public static void main(String[] args) {
-			
-		// teste da classe conta corrente
-		Conta cc1 = new ContaCorrente(2, 123, 1, "Mariana", 15000.0f, 1000.0f);
-		cc1.visualizar();
-		cc1.sacar(12000.0f);
-		cc1.visualizar();
-		cc1.depositar(5000.0f);
-		cc1.visualizar();
-		
-		// teste da classe conta poupança
-		Conta cp1 = new ContaPoupanca(3, 123, 1, "Victor", 100000.0f, 15);
-		cp1.visualizar();
-		cp1.sacar(1000.0f);
-		cp1.visualizar();
-		cp1.depositar(5000.0f);
-		cp1.visualizar();
 		
 		Scanner leia = new Scanner(System.in);
 		int opcao;
